41bb97e9fbb40c46b7bee594fcd7654e39439b12
Kapoutaine <103065787+Kapoutaine@users.noreply.github.com>
2022-12-20 14:56:37 +0000

    Modification des noms de la fctn et des var

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ def accueillir():
     # Rendu de la vue
     return render_template("index.html")
 
-@app.route("/download")
-def download():
+@app.route("/convert")
+def convert():
     """Récupère l'URL entrée par l'utilisateur et la télécharge"""
 
     # Récupération de l'URL
-    url = request.form["url"]
+    link = request.form["link"]
 
     # Rendu de la vue
     """ TODO : ajuster les noms en fonction de la template
